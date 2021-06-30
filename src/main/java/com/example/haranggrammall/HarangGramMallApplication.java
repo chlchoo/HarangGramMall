041bb65aa17d72f12eea578c307420634ed0cd88
choijiyeon <chlwldsu1564@naver.com>
2021-06-30 16:03:48 +0900

    Initial commit

---
+++ src/main/java/com/example/haranggrammall/HarangGramMallApplication.java
@@ -0,0 +1,13 @@
+package com.example.haranggrammall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarangGramMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarangGramMallApplication.class, args);
+    }
+
+}
