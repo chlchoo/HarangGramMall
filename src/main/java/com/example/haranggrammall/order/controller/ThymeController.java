7a209b083cb1006472e3446c270718feb651eec0
choijiyeon <chlwldsu1564@naver.com>
2021-07-12 17:09:10 +0900

    모델명을 넣어줘서 웹페이지에 내가 넣은 값을 볼 수 있게 되었다.

--- src/main/java/com/example/haranggrammall/order/controller/ThymeController.java
+++ src/main/java/com/example/haranggrammall/order/controller/ThymeController.java
@@ -3,15 +3,29 @@ package com.example.haranggrammall.order.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 @Controller
 public class ThymeController {
+
+    private List<Board> boards = Arrays.asList();
+
     @RequestMapping("/")
     String indexPage(Model model){
         Board board = new Board(1, "테스트 제목", "최지연2");
         model.addAttribute("board", board);
         return "board/index";
+    }
+
+    @PostMapping
+    @ResponseBody
+    List<Board>  addPost(Board board) {
+        return boards;
     }
 }
